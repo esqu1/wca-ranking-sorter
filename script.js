5f00ea8d611d52d85bb8cedde79188d4431c73d3
esqu1 <brandbest1@gmail.com>
2017-05-11 22:22:29 -0400

    tabbing

--- script.js
+++ script.js
@@ -11,18 +11,17 @@ $(document).ready(function(){
 	    var head = '<thead><tr><td>Event</td><td>NR</td><td>CR</td><td>WR</td><td>Time</td></tr></thead>';
 	    singleResult.append(head); averageResult.append(head);
 	    for (var i = 0; i < single.length; i++) {
-		var s = $('<tr></tr>').addClass('active');
-		var content = '<td>' + single[i].eventId + '</td><td>' + single[i].countryRank + '</td><td>' + single[i].continentRank + '</td><td><b>' + single[i].worldRank + '</b></td><td>' + (single[i].best * 0.01).toFixed(2) + '</td>';
-		s.append(content);
-		singleResult.append(s);
+			var s = $('<tr></tr>').addClass('active');
+			var content = '<td>' + single[i].eventId + '</td><td>' + single[i].countryRank + '</td><td>' + single[i].continentRank + '</td><td><b>' + single[i].worldRank + '</b></td><td>' + (single[i].best * 0.01).toFixed(2) + '</td>';
+			s.append(content);
+			singleResult.append(s);
 	    }
 	    for (var i = 0; i < average.length; i++) {
-		var s = $('<tr></tr>').addClass('active');
-		var content = '<td>' + average[i].eventId + '</td><td>' + average[i].countryRank + '</td><td>' + average[i].continentRank + '</td><td><b>' + average[i].worldRank + '</b></td><td>' + (average[i].best * 0.01).toFixed(2) + '</td>';
-		s.append(content);
-		averageResult.append(s);
+			var s = $('<tr></tr>').addClass('active');
+			var content = '<td>' + average[i].eventId + '</td><td>' + average[i].countryRank + '</td><td>' + average[i].continentRank + '</td><td><b>' + average[i].worldRank + '</b></td><td>' + (average[i].best * 0.01).toFixed(2) + '</td>';
+			s.append(content);
+			averageResult.append(s);
 	    }
-	    console.log(singleResult);
 	    $('#singleResult').append(singleResult);
 	    $('#averageResult').append(averageResult);
 	});
